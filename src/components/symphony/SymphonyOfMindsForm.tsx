89feeecbce457990b3359b14ef338fd54fdf39c7
Venom120 <yatharthjain120@gmail.com>
2025-05-26 00:58:44 +0530

    temp

--- src/components/symphony/SymphonyOfMindsForm.tsx
+++ src/components/symphony/SymphonyOfMindsForm.tsx
@@ -51,7 +51,7 @@ const SymphonyOfMindsForm = () => {
         </form>
       </Form>
       {/* Support Footer */}
-      <footer className="mt-8 text-center text-2xl text-gray-500 border-t pt-2">
+      <footer className="mt-8 text-center text-xl md:text-2xl text-gray-500 border-t pt-2">
         Support: Dev Team – Yatharth Jain <a href="tel:6264007981" className="underline">6264007981</a>, 
         <a href="mailto:yatharthjain120@gmail.com" className="underline ml-1">yatharthjain120@gmail.com</a>
       </footer>
