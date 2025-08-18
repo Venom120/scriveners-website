f739dd92cb0bdab95d34207a2842940f263e5b00
Venom120 <yatharthjain120@gmail.com>
2025-08-18 17:34:46 +0530

    wildcard allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "127.0.0.1",
     port: 8080,
-    allowedHosts: ["scriveners.venoms.app", "venoms.app", "localhost"],
+    allowedHosts: ["*.venoms.app", "localhost"],
   },
   plugins: [
     react(),
