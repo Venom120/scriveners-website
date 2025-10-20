f4f5b71f80ef61b12aac9d96870c9207c066273c
Venom120 <yatharthjain120@gmail.com>
2025-10-20 14:04:43 +0530

    host removed

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "127.0.0.1",
     port: 8000,
     allowedHosts: ["scriveners.venoms.app"],
   },
